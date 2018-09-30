ebf7f1ffd2457712542c30358310249d5d566a46
Emirhan Soylu <emirhansoylu98@gmail.com>
2018-09-30 19:55:26 +0300

    Tema ve renkler ayarlandı. Single player activity oluşturuldu.

--- app/src/main/java/com/app/codersteam/genelkulturyarismasi/MainActivity.kt
+++ app/src/main/java/com/app/codersteam/genelkulturyarismasi/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.app.codersteam.genelkulturyarismasi
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(View.OnClickListener { it: View ->
+            var intent = Intent(applicationContext,SinglePlayerActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
---
+++ app/src/main/java/com/app/codersteam/genelkulturyarismasi/SinglePlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.app.codersteam.genelkulturyarismasi
+
+import android.app.Activity
+import android.os.Bundle
+
+class SinglePlayerActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_player)
+    }
+}
