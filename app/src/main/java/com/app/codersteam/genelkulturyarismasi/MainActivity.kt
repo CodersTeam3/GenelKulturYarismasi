fd9634f8adcfac80519e48f3fb4ff192c8480489
Burak <ozyurtburak11@gmail.com>
2018-10-16 14:30:34 +0300

    Firebase Authentication and database added Facebook sign in added Google Sign in added User Properties Class added SignInUpActivity.java

--- app/src/main/java/com/app/codersteam/genelkulturyarismasi/MainActivity.kt
+++ app/src/main/java/com/app/codersteam/genelkulturyarismasi/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.app.codersteam.genelkulturyarismasi
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : Activity() {
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        if(auth.currentUser==null){
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/app/codersteam/genelkulturyarismasi/UserProperties.java
@@ -0,0 +1,50 @@
+package com.app.codersteam.genelkulturyarismasi;
+
+public class UserProperties {
+    private String personName;
+    private String personEmail;
+    private String personId ;
+    private String personPhoto;
+    public UserProperties() {
+
+    }
+
+    public UserProperties(String personName, String personEmail, String personId, String personPhoto) {
+        this.personName = personName;
+        this.personEmail = personEmail;
+        this.personId = personId;
+        this.personPhoto = personPhoto;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonEmail() {
+        return personEmail;
+    }
+
+    public void setPersonEmail(String personEmail) {
+        this.personEmail = personEmail;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonPhoto() {
+        return personPhoto;
+    }
+
+    public void setPersonPhoto(String personPhoto) {
+        this.personPhoto = personPhoto;
+    }
+}
