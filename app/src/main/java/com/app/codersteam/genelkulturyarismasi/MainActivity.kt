25f01763155952020dc643eec1b3eb1ed598ef97
Emirhan Soylu <emirhansoylu98@gmail.com>
2018-10-19 22:29:11 +0300

    Para, truva atı, boğaz köprüsü ikonları eklendi.

--- app/src/main/java/com/app/codersteam/genelkulturyarismasi/MainActivity.kt
+++ app/src/main/java/com/app/codersteam/genelkulturyarismasi/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.auth.FirebaseAuth
 
@@ -13,14 +14,29 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener(View.OnClickListener {
-            var intent = Intent(applicationContext,SinglePlayerActivity::class.java)
-            startActivity(intent)
-        })
         auth = FirebaseAuth.getInstance()
         if(auth.currentUser==null){
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
+
     }
+
+    fun buttonPressed(it:View){
+        it as Button
+        when(it.id){
+            singlePlayerButton.id-> {
+                var intent = Intent(applicationContext,SinglePlayerActivity::class.java)
+                startActivity(intent) }
+
+            multiPlayerButton.id-> {Intent(applicationContext,SinglePlayerActivity::class.java)
+                startActivity(intent) }
+
+            else->{
+                Intent(applicationContext,SinglePlayerActivity::class.java)
+                startActivity(intent) }
+
+        }
+    }
+
 }
