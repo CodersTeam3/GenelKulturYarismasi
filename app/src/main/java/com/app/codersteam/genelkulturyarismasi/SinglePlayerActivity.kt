f9353764f30253a67df735589f2bbc229960c0f3
Emirhan Soylu <emirhansoylu98@gmail.com>
2018-10-27 19:14:30 +0300

    Cevaplardan herhangi birine tıklandığında yetenekler eski haline gelebiliyor.

--- app/src/main/java/com/app/codersteam/genelkulturyarismasi/SinglePlayerActivity.kt
+++ app/src/main/java/com/app/codersteam/genelkulturyarismasi/SinglePlayerActivity.kt
@@ -9,38 +9,78 @@ import kotlinx.android.synthetic.main.activity_single_player.*
 
 class SinglePlayerActivity : Activity() {
 
-    var flag:Boolean = true
+    var skillUsed = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_player)
 
         val window = window
         window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-
     }
 
     fun activitySpButtonPressed(it: View){
         when(it.id){
-            btn_skill_half.id->{
+            btn_skill_half.id->{ //Yarı yarıya joker
                 val avd: AnimatedVectorDrawable = getDrawable(R.drawable.avd_skill_half_cancel) as AnimatedVectorDrawable
                 btn_skill_half.setImageDrawable(avd)
                 btn_skill_half.isClickable = false
+                skillUsed = true
                 avd.start()
             }
-            btn_skill_time.id->{
+            btn_skill_time.id->{ //Zaman doldurma joker
                 val avd: AnimatedVectorDrawable = getDrawable(R.drawable.avd_skill_time_cancel) as AnimatedVectorDrawable
                 btn_skill_time.setImageDrawable(avd)
                 btn_skill_time.isClickable = false
+                skillUsed = true
                 avd.start()
             }
-            btn_skill_user.id->{
+            btn_skill_user.id->{ //Kullanıcı cevap oranı joker
                 val avd: AnimatedVectorDrawable = getDrawable(R.drawable.avd_skill_user_cancel) as AnimatedVectorDrawable
                 btn_skill_user.setImageDrawable(avd)
                 btn_skill_user.isClickable = false
+                skillUsed = true
                 avd.start()
             }
+            btn_answer1.id->{ //1.Cevap buttonu
+                if(skillUsed)
+                    refreshSkill()
+            }
+            btn_answer2.id->{ //2.Cevap buttonu
+                if(skillUsed)
+                    refreshSkill()
+            }
+            btn_answer3.id->{ //3.Cevap buttonu
+                if(skillUsed)
+                    refreshSkill()
+            }
+            btn_answer4.id->{ //4.Cevap buttonu
+                if(skillUsed)
+                    refreshSkill()
+            }
+
         }
+    }
+
+    fun refreshSkill(){
+        if (!btn_skill_half.isClickable) {
+                btn_skill_half.isClickable = true
+                val avd: AnimatedVectorDrawable = getDrawable(R.drawable.avd_skill_half_open) as AnimatedVectorDrawable
+                btn_skill_half.setImageDrawable(avd)
+                avd.start()
+            }
+            if (!btn_skill_time.isClickable) {
+                btn_skill_time.isClickable = true
+                val avd: AnimatedVectorDrawable = getDrawable(R.drawable.avd_skill_time_open) as AnimatedVectorDrawable
+                btn_skill_time.setImageDrawable(avd)
+                avd.start()
+            }
+            if (!btn_skill_user.isClickable) {
+                btn_skill_user.isClickable = true
+                val avd: AnimatedVectorDrawable = getDrawable(R.drawable.avd_skill_user_open) as AnimatedVectorDrawable
+                btn_skill_user.setImageDrawable(avd)
+                avd.start()
+            }
+            skillUsed = false
     }
 }
